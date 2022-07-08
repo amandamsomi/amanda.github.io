f98e20be27caacc6bd9d5dde1907480e375d2773
amandamsomi <108739301+amandamsomi@users.noreply.github.com>
2022-07-08 15:20:38 +0200

    Add files via upload
    .

---
+++ assets/img/img/app.js
@@ -0,0 +1,20 @@
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('çolour1');
+    document.body.classList.toggle('colour2');
+
+
+
+
+
+const className = Document.body.className;
+if(className == "colour2")
+{
+    this.textContent = "colour2";
+}
+else
+{
+    this.textContent ="colour1"
+}
+});
